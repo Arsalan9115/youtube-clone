153faadc118b7ad83becaafd65f5d323a82f2b45
Arsalan9115 <arsalanbotawala256@gmail.com>
2026-04-30 19:16:56 +0530

    Update axiosinstance.js

--- yourtube/src/lib/axiosinstance.js
+++ yourtube/src/lib/axiosinstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://youtube-clone-backend-api.onrender.com",
-  withCredentials: true,
+  withCredentials: faslse,
 });
 
 export default axiosInstance;
