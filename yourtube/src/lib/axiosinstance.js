8241dd0ae00affa010ff2c7d0cb2628cbf5ba1bc
Arsalan9115 <arsalanbotawala256@gmail.com>
2026-04-30 06:04:21 +0530

    Update axiosinstance.js

--- yourtube/src/lib/axiosinstance.js
+++ yourtube/src/lib/axiosinstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 export default axiosInstance;
